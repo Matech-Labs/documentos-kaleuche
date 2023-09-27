76a534e6a5ecff2e086a1bb7150e5c24cdfd4eb0
ManuOreguy <60889727+ManuOreguy@users.noreply.github.com>
2023-09-27 20:40:56 -0300

    New dalessio fixes

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,13 @@
-import styles from "./page.module.scss";
+import "./scss/index.scss";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <title>Documentos Kaleuche</title>
+        {/* <link rel="icon" href="/favicon.png" /> */}
       </head>
-      <body className={styles.bodyContainer}>
-        <h1 className={styles.mainTitle}>Documentos Kaleuche</h1>
-        <div className={styles.subtitle}>
-          <p>
-            De está página vas a poder descargar, de primera mano, toda la
-            documentación que acredita la titularidad del Fideicomiso
-            Inmobiliario Nuevo Kaleuche sobre todos aquellos lotes que
-            integraron la quiebra de Juan Claudio Chavanne y hoy se encuentran
-            registrados a nombre de Jorge Tejerina, Marcelo Augusto Chavanne, La
-            Vertiente Andina S.A., Santiago Palmieri y Carlos Federico Chavanne.
-            De esta forma vas a poder contar con todos los elementos necesarios
-            para analizar tu situación.
-          </p>
-          <p>
-            También vas a poder acceder a los informes de dominio de todos esos
-            lotes.
-          </p>
-        </div>
+      <body>
         <main>{children}</main>
       </body>
     </html>
